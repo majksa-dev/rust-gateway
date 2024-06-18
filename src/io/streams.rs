63310fd7e9b4e865457c86580743187e6775c11d
Ondřej Maxa <ondrej.maxa11@gmail.com>
2024-06-18 14:52:01 +0200

    refactor: remove unused function

--- src/io/streams.rs
+++ src/io/streams.rs
@@ -1,31 +1,6 @@
 use std::io::{Read, Write};
 
-use tokio::{
-    io::{self, AsyncRead, AsyncWrite},
-    task::JoinHandle,
-};
-
-pub trait WriteReaderAsync {
-    fn write_reader<R>(self, reader: R) -> JoinHandle<()>
-    where
-        R: AsyncRead + Unpin + Send + 'static;
-}
-
-impl<W> WriteReaderAsync for W
-where
-    W: AsyncWrite + Unpin + Send + 'static,
-{
-    fn write_reader<R>(mut self, mut reader: R) -> JoinHandle<()>
-    where
-        R: AsyncRead + Unpin + Send + 'static,
-    {
-        tokio::spawn(async move {
-            if let Err(err) = io::copy(&mut reader, &mut self).await {
-                essentials::warn!("Failed to copy stream: {:?}", err);
-            }
-        })
-    }
-}
+use tokio::task::JoinHandle;
 
 pub trait WriteReader {
     fn write_reader<R>(self, reader: R) -> JoinHandle<()>
